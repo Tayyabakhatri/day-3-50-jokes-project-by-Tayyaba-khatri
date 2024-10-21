f517d460d1039267557628983a329ab23c84ebc5
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-10-21 23:26:40 +0500

    modified

--- app.js
+++ app.js
@@ -14,23 +14,59 @@
 //     }
 //   }
 //   abc();
-
-async function abc() {
-    try {
-
-        let awai = await fetch('https://www.urdupoint.com/kids/category/jokes')
-        if(!awai.ok){
-            throw new Error('Network response was not OK');
-        }
-        const data = await awai.json()
-        console.log(data)
-        let joke = document.getElementById('joke')
-        joke.innerHTML = data.message
-} catch(error) {
-        console.error('Error:', error);
+let jokes = [
+    {
+        joke1: "joke 1"
+    },
+    {
+        joke2: "joke 2"
+    },
+    {
+        joke3: "joke 3"
+    },
+    {
+        joke4: "joke 4"
     }
+]
+let index = 0
+
+
+function showJokes(){
+    let jokeDiv = document.getElementById("joke");
+    // console.log(oneByOne)
+    for(let key of jokes){
+        console.log(key)
+        jokeDiv.innerHTML=jokes[index]
+    }
+    // jokes.forEach((joke,()=>{
+    //     let oneByOne = jokes[index]
+    //     jokeDiv.innerHTML=oneByOne
+    // }))
 }
-let btn = document.getElementById('btn')
-btn.addEventListener("click",abc)
+showJokes()
+let nextBtn = document.getElementById("btn")
+nextBtn.addEventListener('click',showJokes)
+
+// async function abc() {
+//     let index=0
+//     try {
+//         let jokeDiv=document.getElementById("joke");
+//         let oneByOne = jokes[index]
+//         console.log(oneByOne)
+
+// let awai = await fetch('https://www.urdupoint.com/kids/category/jokes')
+// if (!awai.ok) {
+//     throw new Error('Network response was not OK');
+// }
+// const data = await awai.json()
+// console.log(data)
+// let joke = document.getElementById('joke')
+// joke.innerHTML = data.message
+//     } catch (error) {
+//         console.error('Error:', error);
+//     }
+// }
+// let btn = document.getElementById('btn')
+// btn.addEventListener("click", abc)
 
 
