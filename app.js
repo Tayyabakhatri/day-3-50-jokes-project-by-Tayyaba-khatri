4c5715f9266f7e6ee72f62ebce7b50068ac61f2c
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-10-22 22:21:56 +0500

    modified

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
-// userName = "tayyaba"
-// let key = "id"
-// let obj = {
-//     userName,
-//     [key]: 9
-// }
-// console.log(obj)
-// async function abc() {
-//     try {
-//       const response = await fetch('https://www.wix.com');
-//       console.log(response);  // Will not contain useful information
-//     } catch (error) {
-//       console.error('Fetch error:', error);
-//     }
-//   }
-//   abc();
-
 let jokes = [
     {
-        joke: "joke 1"
+        joke: "ایک خاتون نے دوسری خاتون سے پو چھا۔”جنت میں مردوں کو تو حوریں ملیں گی مگر عورتوں کو کیا ملے گا؟“ ”یہی اپنے اپنے خاوند۔“ دوسری عورت نے جواب میں بتایا۔ پہلی عورت نے یہ سنا تو تلخی سے بولی۔”دفع کرو پھر مرنے کا کیا فائدہ۔“"
     },
     {
         joke: "joke 2"
@@ -32,65 +15,15 @@ let jokes = [
 let index = 0
 
 
-function showJokes(){
+function showJokes() {
     let jokeDiv = document.getElementById("joke");
-    // for(let key of jokes){
-        console.log(key)
-        jokeDiv.innerHTML=jokes[index].joke
-    // }
-
-showJokes()
-
+    jokeDiv.className="jokeFont"
+    jokeDiv.innerHTML = jokes[index].joke
 }
+showJokes()
 let nextBtn = document.getElementById("btn")
-nextBtn.addEventListener('click',()=>{
-    index=(index+1)%jokes.length
+nextBtn.addEventListener('click', () => {
+    index = (index + 1) % jokes.length
     showJokes()
 })
-// async function abc() {
-//     let index=0
-//     try {
-//         let jokeDiv=document.getElementById("joke");
-//         let oneByOne = jokes[index]
-//         console.log(oneByOne)
-
-// let awai = await fetch('https://www.urdupoint.com/kids/category/jokes')
-// if (!awai.ok) {
-//     throw new Error('Network response was not OK');
-// }
-// const data = await awai.json()
-// console.log(data)
-// let joke = document.getElementById('joke')
-// joke.innerHTML = data.message
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-// }
-// let btn = document.getElementById('btn')
-// btn.addEventListener("click", abc)
-
-
-
-
-// let jokes = [
-//     { joke: "joke 1" },
-//     { joke: "joke 2" },
-//     { joke: "joke 3" },
-//     { joke: "joke 4" }
-// ];
-
-// let index = 0;
-
-// function showJokes() {
-//     let jokeDiv = document.getElementById("joke");
-//     jokeDiv.innerHTML = jokes[index].joke; // Display the joke at the current index
-// }
-
-// showJokes(); // Show the first joke initially
-
-// let nextBtn = document.getElementById("btn");
-// nextBtn.addEventListener('click', () => {
-//     index = (index + 1) % jokes.length; // Increment index and wrap around if it exceeds array length
-//     showJokes(); // Display the next joke
-// });
 
